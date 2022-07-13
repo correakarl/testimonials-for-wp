77f54a850c38507da5cac50ee5a545541d032764
correakarl <karl.correa.88@gmail.com>
2022-07-13 07:43:59 -0400

    base file

---
+++ testimonials-for-wp.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Testimonials for WordPress - by CorreaKarl
+ * Plugin URI: https://#
+ * Description: Plugin generator of shorcuts for user testimonials.
+ * Version: 1.0
+ * Author: Karl Correa
+ * Author URI: https://#
+ */
